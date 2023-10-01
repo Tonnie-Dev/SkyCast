25ea778cebfa592231c13283a3a06dd049ddedb9
Tonnie <vontonnie@gmail.com>
2023-10-01 12:50:56 +0300

    Add Constants File

---
+++ app/src/main/java/com/uxstate/skycast/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.uxstate.skycast.utils
+
+// TODO: Move base URL to gradle build config
+
+
+//BASE URL
+const val BASE_URL = "https://api.openweathermap.org/"
+
+//Endpoints
+const val GET_CURRENT= "data/2.5/weather"
+const val GET_FORECAST = "data/2.5/forecast"
