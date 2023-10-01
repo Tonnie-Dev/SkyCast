b41e295e132cb90a22bb7713e98187b40a084fd0
Tonnie <vontonnie@gmail.com>
2023-10-01 19:37:27 +0300

    Add WeatherDao class

---
+++ app/src/main/java/com/uxstate/skycast/data/local/dao/WeatherDao.kt
@@ -0,0 +1,31 @@
+package com.uxstate.skycast.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.uxstate.skycast.data.local.entity.CurrentEntity
+import com.uxstate.skycast.data.local.entity.ForecastEntity
+
+@Dao
+interface WeatherDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCurrentData(currentData:CurrentEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertForecastData(forecastData:List<ForecastEntity>)
+
+    @Query("DELETE FROM current_weather_table")
+    suspend fun clearCurrentWeatherData()
+
+    @Query("DELETE FROM forecast_weather_table")
+    suspend fun clearForecastWeatherData()
+
+    @Query("SELECT * FROM current_weather_table")
+    suspend fun getCurrentWeather():CurrentEntity?
+
+    @Query("SELECT* FROM forecast_weather_table")
+    suspend fun getForecastWeather():ForecastEntity?
+
+}
