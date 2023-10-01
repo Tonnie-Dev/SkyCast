293ed84bda9543ee8cd63a44c3307d02c7720699
Tonnie <vontonnie@gmail.com>
2023-10-01 19:45:31 +0300

    Add WeatherDatabase class

---
+++ app/src/main/java/com/uxstate/skycast/data/local/db/WeatherDatabase.kt
@@ -0,0 +1,26 @@
+package com.uxstate.skycast.data.local.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.uxstate.skycast.data.local.dao.WeatherDao
+import com.uxstate.skycast.data.local.db.WeatherDatabase.Companion.DATABASE_VERSION
+import com.uxstate.skycast.data.local.db.WeatherDatabase.Companion.IS_EXPORT_SCHEMA
+import com.uxstate.skycast.data.local.entity.CurrentEntity
+import com.uxstate.skycast.data.local.entity.ForecastEntity
+
+@Database(
+        entities = [CurrentEntity::class, ForecastEntity::class],
+        version = DATABASE_VERSION,
+        exportSchema = IS_EXPORT_SCHEMA
+)
+abstract class WeatherDatabase : RoomDatabase() {
+
+    abstract val dao: WeatherDao
+
+    companion object {
+
+        const val DATABASE_NAME = "weather_database"
+        const val IS_EXPORT_SCHEMA = false
+        const val DATABASE_VERSION = 1
+    }
+}
