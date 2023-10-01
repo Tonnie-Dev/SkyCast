c20661ecd12deae36539a80403e6fcecf796b9b2
Tonnie <vontonnie@gmail.com>
2023-10-01 06:26:50 +0300

    Add refreshVersions inside settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,21 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
+}
+
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.60.2"
+
 }
 
 rootProject.name = "SkyCast"
