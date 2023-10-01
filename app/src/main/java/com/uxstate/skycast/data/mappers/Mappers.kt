b043b0a32bfc8f2339ef5d4fe298fe424c7a4379
Tonnie <vontonnie@gmail.com>
2023-10-01 17:21:00 +0300

    Add CurrentWeatherDto.toEntity() model functionN

--- app/src/main/java/com/uxstate/skycast/data/mappers/Mappers.kt
+++ app/src/main/java/com/uxstate/skycast/data/mappers/Mappers.kt
@@ -3,18 +3,19 @@ package com.uxstate.skycast.data.mappers
 import com.uxstate.skycast.data.local.entity.CurrentEntity
 import com.uxstate.skycast.data.remote.dto.current.CurrentWeatherDto
 import com.uxstate.skycast.data.remote.dto.current.WeatherDescriptionDto
+import com.uxstate.skycast.data.remote.dto.current.WindDto
 import com.uxstate.skycast.domain.model.WeatherCondition
 import com.uxstate.skycast.domain.model.WeatherDescription
 import com.uxstate.skycast.domain.model.Wind
 
-fun CurrentWeatherDto.toEntity(lastFetchTime:Long):CurrentEntity {
+fun CurrentWeatherDto.toEntity(lastFetchTime: Long): CurrentEntity {
 
     return CurrentEntity(
             lastFetchTime = lastFetchTime,
             cityName = this.name,
             cityId = this.id,
             weatherDesc = this.weather.map { it.toModel() },
-            wind =this.wind,
+            wind = this.wind.toModel(),
             weatherCondition = WeatherCondition(
                     temp = this.main.temp,
                     pressure = this.main.pressure.toDouble(),
@@ -24,9 +25,11 @@ fun CurrentWeatherDto.toEntity(lastFetchTime:Long):CurrentEntity {
 }
 
 
-fun WeatherDescriptionDto.toModel():WeatherDescription = WeatherDescription(
+fun WeatherDescriptionDto.toModel(): WeatherDescription = WeatherDescription(
         id = this.id,
         main = this.main,
         description = this.description,
         icon = this.icon
 )
+
+fun WindDto.toModel(): Wind = Wind(speed = this.speed, deg = this.deg)
