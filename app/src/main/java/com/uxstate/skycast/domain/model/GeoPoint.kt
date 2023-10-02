6154437b87b2a6c089daa5abe046d3e418a3d24d
Tonnie <vontonnie@gmail.com>
2023-10-02 18:26:22 +0300

    Add RemoteDataSource inteface

---
+++ app/src/main/java/com/uxstate/skycast/domain/model/GeoPoint.kt
@@ -0,0 +1,3 @@
+package com.uxstate.skycast.domain.model
+
+data class GeoPoint(val latitude:Double, val longitude:Double)
--- app/src/main/java/com/uxstate/skycast/domain/model/LocationCoor.kt
+++
@@ -1,3 +0,0 @@
-package com.uxstate.skycast.domain.model
-
-data class LocationCoor(val latitude:Double, val longitude:Double)
