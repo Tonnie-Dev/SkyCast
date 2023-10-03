03aedb2744998e28873c5f1e316e7c40ad77c438
Tonnie <vontonnie@gmail.com>
2023-10-03 08:11:48 +0300

    Add JsonParserImpl for Moshi

---
+++ app/src/main/java/com/uxstate/skycast/data/local/converter/JsonParserImpl.kt
@@ -0,0 +1,26 @@
+package com.uxstate.skycast.data.local.converter
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import java.lang.reflect.Type
+import javax.inject.Inject
+
+class JsonParserImpl @Inject constructor(private val moshi: Moshi) : JsonParser {
+
+
+   
+    //Write Object to Room String
+    override fun <T> toJson(obj: T, type: Type): String? {
+        //use jsonAdapter<T> for generic adapter
+        val jsonAdapter: JsonAdapter<T> = moshi.adapter(type)
+
+        return jsonAdapter.toJson(obj)
+    }
+
+    //Read Object from Room String
+    override fun <T> fromJson(json: String, type: Type): T? {
+        //use jsonAdapter<T> for generic adapter
+        val jsonAdapter: JsonAdapter<T> = moshi.adapter(type)
+        return jsonAdapter.fromJson(json)
+    }
+}
