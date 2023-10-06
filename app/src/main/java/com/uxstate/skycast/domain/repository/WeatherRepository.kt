fd5fa737309f81aabee5e9fa9219e772b5566717
Tonnie <vontonnie@gmail.com>
2023-10-06 07:40:10 +0300

    Add WeatherRepository interface class

---
+++ app/src/main/java/com/uxstate/skycast/domain/repository/WeatherRepository.kt
@@ -0,0 +1,13 @@
+package com.uxstate.skycast.domain.repository
+
+import com.uxstate.skycast.domain.model.CurrentWeather
+import com.uxstate.skycast.domain.model.ForecastWeather
+import com.uxstate.skycast.domain.model.GeoPoint
+import com.uxstate.skycast.utils.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface WeatherRepository {
+
+  fun getCurrentWeather(geoPoint: GeoPoint): Flow<Resource<CurrentWeather>>
+  fun getForecastWeather(cityId:Int):Flow<Resource<List<ForecastWeather>>>
+}
