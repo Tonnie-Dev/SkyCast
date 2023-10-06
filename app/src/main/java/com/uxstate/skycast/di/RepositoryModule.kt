eaa3960ec0c9ce628275cce03a40abb2b092ddef
Tonnie <vontonnie@gmail.com>
2023-10-06 07:50:16 +0300

    Add RepositoryModule abstract class

---
+++ app/src/main/java/com/uxstate/skycast/di/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.uxstate.skycast.di
+
+import com.uxstate.skycast.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindRepository(weatherRepositoryImpl: WeatherRepository): WeatherRepository
+}
