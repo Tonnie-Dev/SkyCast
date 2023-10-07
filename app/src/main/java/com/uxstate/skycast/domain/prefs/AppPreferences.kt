3882582b55d550b317fb54c00552b2a55f51eb70
Tonnie <vontonnie@gmail.com>
2023-10-07 23:40:57 +0300

    Add AppPrefrences class

--- app/src/main/java/com/uxstate/skycast/domain/prefs/AppPreferences.kt
+++ app/src/main/java/com/uxstate/skycast/domain/prefs/AppPreferences.kt
@@ -3,17 +3,23 @@ package com.uxstate.skycast.domain.prefs
 import androidx.annotation.StringRes
 import com.uxstate.skycast.R
 
-enum class Theme(@StringRes val themeName:Int){
+enum class Theme(@StringRes val themeName: Int) {
 
     SYSTEM(R.string.system_default),
     LIGHT(R.string.light),
     DARK(R.string.dark)
 }
 
-enum class TempUnit(@StringRes val tempUnit:Int){
+enum class TempUnit(@StringRes val tempUnit: Int) {
 
     FAHRENHEIT(R.string.unit_fahrenheit),
     CELSIUS(R.string.unit_celsius)
 
 
 }
+
+data class AppPreferences(
+    val theme: Theme,
+    val tempUnit: TempUnit,
+    val savedCityId: Int
+)
