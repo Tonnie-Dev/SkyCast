6b5e6eae9b27ff532dce3dc6d8f24a947909a716
Tonnie <vontonnie@gmail.com>
2023-10-08 15:18:31 +0300

    Add saveCityId HomeViewModel function

--- app/src/main/java/com/uxstate/skycast/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/uxstate/skycast/presentation/home/HomeViewModel.kt
@@ -1,10 +1,14 @@
 package com.uxstate.skycast.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uxstate.skycast.domain.location.LocationTracker
 import com.uxstate.skycast.domain.prefs.DataStoreOperations
 import com.uxstate.skycast.domain.repository.WeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,5 +18,13 @@ class HomeViewModel @Inject constructor(
     val prefs: DataStoreOperations
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(HomeState)
+    val uiState = _uiState.asStateFlow()
 
+    private fun saveCityId(cityId:Int){
+
+        viewModelScope.launch {
+            prefs.updateCityId(cityId = cityId)
+        }
+    }
 }
