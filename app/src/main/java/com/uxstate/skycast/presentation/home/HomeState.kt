f11a3687d8332e89598d1ec1481e0ef016ff5f63
Tonnie <vontonnie@gmail.com>
2023-10-08 15:14:12 +0300

    Add HomeState data class

---
+++ app/src/main/java/com/uxstate/skycast/presentation/home/HomeState.kt
@@ -0,0 +1,23 @@
+package com.uxstate.skycast.presentation.home
+
+import com.uxstate.skycast.domain.model.CurrentWeather
+import com.uxstate.skycast.domain.prefs.AppPreferences
+import com.uxstate.skycast.domain.prefs.TempUnit
+import com.uxstate.skycast.domain.prefs.Theme
+
+data class HomeState(
+    val currentWeather: CurrentWeather? = null,
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val appPreferences: AppPreferences = appPreferencesInitialState
+) {
+
+    companion object {
+        val appPreferencesInitialState =
+            AppPreferences(
+                    theme = Theme.SYSTEM,
+                    tempUnit = TempUnit.CELSIUS,
+                    savedCityId = -1
+            )
+    }
+}
