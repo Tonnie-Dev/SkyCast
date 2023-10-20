794d38ab1bab5ada170a42e2fbdcfcae978caed8
Tonnie <vontonnie@gmail.com>
2023-10-20 19:46:39 +0300

    Apply DarkTheme Logic

--- app/src/main/java/com/uxstate/skycast/MainActivity.kt
+++ app/src/main/java/com/uxstate/skycast/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.DestinationsNavHost
+import com.uxstate.skycast.domain.prefs.Theme
 import com.uxstate.skycast.presentation.NavGraphs
+import com.uxstate.skycast.presentation.settings.SettingsViewModel
 //import com.uxstate.skycast.presentation.NavGraphs
 import com.uxstate.skycast.ui.theme.SkyCastTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,8 +33,22 @@ class MainActivity : ComponentActivity() {
                         Color.TRANSPARENT
                 ), navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
         )
+
+
         setContent {
-            SkyCastTheme {
+
+            val viewModel:SettingsViewModel = hiltViewModel()
+            val state by viewModel.state.collectAsState()
+
+            val prefs = state.appPreferences
+
+            val isDark = when(prefs.theme){
+
+                Theme.SYSTEM -> isSystemInDarkTheme()
+                Theme.DARK -> true
+                Theme.LIGHT -> false
+            }
+            SkyCastTheme(darkTheme = isDark) {
                 // A surface container using the 'background' color from the theme
                 Surface(
                         modifier = Modifier.fillMaxSize(),
