5e986b4a7544ace4add076b50a49292edf03ef6d
Tonnie <vontonnie@gmail.com>
2023-11-20 19:25:36 +0300

    Add gifs recordings

--- app/src/main/java/com/uxstate/skycast/presentation/MainActivity.kt
+++ app/src/main/java/com/uxstate/skycast/presentation/MainActivity.kt
@@ -34,10 +34,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         installSplashScreen().apply {
             setKeepOnScreenCondition{
-//true
-               !viewModel.state.value.isLoading
+
+               false
             }
         }
         enableEdgeToEdge(
